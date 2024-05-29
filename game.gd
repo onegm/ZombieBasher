3f768150c7d1f29dd2df9d57498c87b820226b8e
Omar - Home <negmok@gmail.com>
2024-05-28 22:00:27 -0400

    Add super slime

--- game.gd
+++ game.gd
@@ -1,23 +1,23 @@
 extends Node2D
 
 @export
-var mob_scene : PackedScene
+var enemy_scene : PackedScene
 
 @onready
 var spawn_path = $Player/SpawnPath/PathFollow2D
 
 func _ready():
-	$Timer.timeout.connect(spawn_mob)
+	$Timer.timeout.connect(spawn_enemy)
 	$Player.health_depleted.connect(on_player_health_depleted)
 	for i in range(7):
-		spawn_mob()
+		spawn_enemy()
 
-func spawn_mob():
-	var mob = mob_scene.instantiate()
+func spawn_enemy():
+	var enemy = enemy_scene.instantiate()
 	spawn_path.progress_ratio = randf()
-	mob.global_position = spawn_path.global_position
-	mob.player = $Player
-	add_child(mob)
+	enemy.global_position = spawn_path.global_position
+	enemy.player = $Player
+	add_child(enemy)
 
 func on_player_health_depleted():
 	$GameOverLayer.set_visible(true)
