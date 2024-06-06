4f5a6ecb06311f49a1f44af47bfc0fcad6e2cf14
Omar - Home <negmok@gmail.com>
2024-06-06 06:51:17 -0400

    create flashlight

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 class_name Player
 
-var speed = 600
-var health = 1.0
+var max_speed = 500
+var acceleration = 1000
+var deceleration = 3000
+var health = 100.0
 var damage_rate = 5.0
 var current_weapon : Weapon = null
 
@@ -11,11 +13,16 @@ signal health_depleted
 @onready
 var animations : Node2D = $Animations
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction == Vector2.ZERO:
+		velocity -= velocity.normalized()* deceleration * delta
+	else:
+		velocity += direction * acceleration * delta
 	
-	velocity = direction * speed
+	velocity = velocity.clamp(Vector2(-1,-1)*max_speed, Vector2.ONE*max_speed)
 	animations.look_at(get_global_mouse_position())
+	#$FlashLight.look_at(get_global_mouse_position())
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
